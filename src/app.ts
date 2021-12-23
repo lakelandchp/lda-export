0df3571bea148bd73f892d8a608de52ccfde57ee
Trevor Muñoz <trevor.munoz@gmail.com>
2021-12-23 14:30:17 -0500

    Basic version of core functions for retrieving data
    TODO: munge and write to file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,114 @@
-const base = Deno.env.get("AIRTABLE_BASE_ID");
-console.log(`Data will be exported from base: ${base}`);
+import {
+  brightGreen,
+  cyan,
+  italic,
+  yellow,
+} from "https://deno.land/std@0.119.0/fmt/colors.ts";
+import { sleep } from "https://deno.land/x/sleep/mod.ts";
 
-type AirtableRecord = Record<string, any>;
+type AirtableRecord = Record<string, unknown>;
 
-function allRecords(
+async function getAirtableData(
+  baseId: string,
+  tables: string[],
+  apiKey: string,
+  httpReadTimeout?: number,
+  userAgent?: string | null,
+  delay = 0.2,
+  outputDir = "./public",
+): Promise<void> {
+  console.log(
+    `Connecting to ${cyan(`https://api.airtable.com/v0/${baseId}`)} …`,
+  );
+
+  for (const table of tables) {
+    console.log(`Fetching ${yellow(table)} …`);
+    // The function allRecords() returns a generator that yields  a "page" of
+    // records returned by the API. We push these onto our array as they are
+    // yielded so that's why we have a list of lists here.
+    //
+    // Below we use flat() to flatten the list of lists into a single list.
+    const records: Array<AirtableRecord[]> = [];
+
+    const tableRecords = allRecords(
+      baseId,
+      table,
+      apiKey,
+      httpReadTimeout,
+      userAgent,
+      delay,
+    );
+
+    for await (const recordList of tableRecords) {
+      records.push(recordList);
+    }
+
+    const res = records.flat();
+    console.log(
+      `🗄✨ ${italic(brightGreen(res.length.toString()))} records retrieved.`,
+    );
+  }
+
+  console.log(`Records will be written to ${cyan(outputDir)}`);
+}
+
+async function* allRecords(
   baseId: string,
   tableName: string,
   apiKey: string,
   httpReadTimeout?: number,
   userAgent?: string | null,
-  sleep: number = 0.2,
-): Iterable<AirtableRecord> {
+  delay?: number,
+): AsyncGenerator<AirtableRecord[], void, void> {
+  //   Prepare the request
+  const _headers: Array<string[]> = [];
+  if (userAgent) {
+    _headers.push(["User-Agent", userAgent]);
+  }
+  _headers.push(["Authorization", `Bearer ${apiKey}`]);
+  const requestHeaders = new Headers(_headers);
+
+  let timeout = 8000;
+  if (httpReadTimeout) {
+    timeout = httpReadTimeout * 1000;
+  }
+
+  const controller = new AbortController();
+  const cId = setTimeout(() => controller.abort(), timeout);
+
+  // Make the request
+  let firstPage = true;
+  let offset: null | string = null;
+  while (firstPage || offset) {
+    firstPage = false;
+    let url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
+    if (offset) {
+      url += `?offset=${offset}`;
+    }
+
+    const jsonResponse = await fetch(url, {
+      headers: requestHeaders,
+      signal: controller.signal,
+    });
+    const jsonData = await jsonResponse.json();
+    offset = jsonData.offset;
+    yield new Promise<AirtableRecord[]>((resolve) => resolve(jsonData.records));
+
+    if (offset && delay) {
+      await sleep(delay);
+    }
+  }
+
+  clearTimeout(cId);
+}
+
+const base = Deno.env.get("AIRTABLE_BASE_ID");
+const key = Deno.env.get("AIRTABLE_API_KEY");
+
+const tables = ["Items", "Item_Admin_Info"];
+
+if (base && key) {
+  getAirtableData(base, tables, key, 60, "curl/7.77.0");
+} else {
+  console.error("Error: AIRTABLE_BASE_ID and AIRTABLE_API_KEY must be set");
 }
