de5f164e073882a682afadabfdcf86dc61dda1db
Trevor Muñoz <trevor.munoz@gmail.com>
2021-12-26 15:50:26 -0500

    Add a couple more Airtable types

--- src/airtable.ts
+++ src/airtable.ts
@@ -4,3 +4,15 @@ type AirtableAPIAllowedKeys =
   | "fields";
 
 type AirtableRecord = Record<AirtableAPIAllowedKeys, unknown>;
+
+type AttachmentAllowedKeys =
+  | "id"
+  | "filename"
+  | "url"
+  | "thumbnails"
+  | "width"
+  | "height"
+  | "type"
+  | "size";
+
+type AirtableAttachment = Record<AttachmentAllowedKeys, unknown>;
