8b35396a6ade0ad80b67b54cfc21fb0b0c83f62b
Trevor Muñoz <trevor.munoz@gmail.com>
2022-01-15 12:50:54 -0500

    Create a deps.ts file

---
+++ src/deps.ts
@@ -0,0 +1,11 @@
+export {
+  brightGreen,
+  cyan,
+  italic,
+  red,
+  yellow,
+} from "https://deno.land/std@0.119.0/fmt/colors.ts";
+export { parse } from "https://deno.land/std@0.119.0/flags/mod.ts";
+export { sleep } from "https://deno.land/x/sleep/mod.ts";
+export { join } from "https://deno.land/std@0.119.0/path/mod.ts";
+export { ensureDirSync } from "https://deno.land/std@0.119.0/fs/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import {
   brightGreen,
   cyan,
+  ensureDirSync,
   italic,
+  join,
+  parse,
   red,
+  sleep,
   yellow,
-} from "https://deno.land/std@0.119.0/fmt/colors.ts";
-import { parse } from "https://deno.land/std@0.119.0/flags/mod.ts";
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
-import { join } from "https://deno.land/std@0.119.0/path/mod.ts";
-import { ensureDirSync } from "https://deno.land/std@0.119.0/fs/mod.ts";
+} from "./deps.ts";
 import { AirtableRecord } from "./airtable.ts";
 import { reshape } from "./transform.ts";
 
