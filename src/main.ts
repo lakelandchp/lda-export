205a2612bd7a6792de2de35dd0c09a224687b8a1
Trevor Muñoz <trevor.munoz@gmail.com>
2024-08-16 07:13:42 -0400

    refactor: inject fetch dependency to ease testing

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,27 @@ import { sleep } from "sleep";
 import { join } from "std/path";
 import { ensureDirSync } from "std/fs";
 
-async function getAirtableData(
+interface FetchOptions {
+  baseId: string;
+  tableName: string;
+  apiKey: string;
+  httpReadTimeout?: number;
+  userAgent?: string | null;
+  delay?: number;
+}
+
+interface AirtableResponse {
+  records: Record<string, unknown>[];
+  offset?: string;
+}
+export async function getAirtableData(
   baseId: string,
   tables: string[],
   apiKey: string,
   httpReadTimeout: number,
+  fetchFn: typeof fetch,
+  logger: typeof console,
+
   userAgent?: string | null,
   delay = 0.2,
 ): Promise<Map<string, Record<string, unknown>[]>> {
@@ -20,7 +36,7 @@ async function getAirtableData(
 
   // Main loop over all tables
   for (const table of tables) {
-    console.log(`Fetching ${yellow(table)} …`);
+    logger.log(`Fetching ${yellow(table)} …`);
     // The function allRecords() returns a generator that yields  a "page" of
     // records returned by the API. We push these onto our array as they are
     // yielded so that's why we have a list of lists here.
@@ -29,12 +45,15 @@ async function getAirtableData(
     const records: Array<Record<string, unknown>[]> = [];
 
     const tableRecords = allRecords(
-      baseId,
-      table,
-      apiKey,
-      httpReadTimeout,
-      userAgent,
-      delay,
+      {
+        baseId,
+        tableName: table,
+        apiKey,
+        httpReadTimeout,
+        userAgent,
+        delay,
+      },
+      fetchFn,
     );
 
     for await (const recordList of tableRecords) {
@@ -42,7 +61,7 @@ async function getAirtableData(
     }
 
     const res = records.flat();
-    console.log(
+    logger.log(
       `🗄✨ ${italic(brightGreen(res.length.toString()))} records retrieved.`,
     );
 
@@ -58,13 +77,11 @@ async function getAirtableData(
 }
 
 async function* allRecords(
-  baseId: string,
-  tableName: string,
-  apiKey: string,
-  httpReadTimeout?: number,
-  userAgent?: string | null,
-  delay?: number,
+  options: FetchOptions,
+  fetchFn: typeof fetch,
 ): AsyncGenerator<Record<string, unknown>[], void, void> {
+  const { baseId, tableName, apiKey, httpReadTimeout, userAgent, delay } =
+    options;
   //   Prepare the request
   const _headers: Array<string[]> = [];
   if (userAgent) {
@@ -73,46 +90,46 @@ async function* allRecords(
   _headers.push(["Authorization", `Bearer ${apiKey}`]);
   const requestHeaders = new Headers(_headers);
 
-  let timeout = 8000;
-  if (httpReadTimeout) {
-    timeout = httpReadTimeout * 1000;
-  }
-
+  const timeout = (httpReadTimeout || 8) * 1000;
   const controller = new AbortController();
   const cId = setTimeout(() => controller.abort(), timeout);
 
   // Make the request
   let firstPage = true;
-  let offset: null | string = null;
+  let offset: string | undefined;
   while (firstPage || offset) {
     firstPage = false;
     let url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
     if (offset) {
       url += `?offset=${offset}`;
     }
+    try {
+      const jsonResponse = await fetchFn(url, {
+        headers: requestHeaders,
+        signal: controller.signal,
+      });
 
-    const jsonResponse = await fetch(url, {
-      headers: requestHeaders,
-      signal: controller.signal,
-    });
-    const jsonData = await jsonResponse.json();
-    offset = jsonData.offset;
-    yield new Promise<Record<string, unknown>[]>((resolve) =>
-      resolve(jsonData.records),
-    );
+      const jsonData: AirtableResponse = await jsonResponse.json();
+      yield jsonData.records;
+      offset = jsonData.offset;
 
-    // Don't hit the API too fast
-    if (offset && delay) {
-      await sleep(delay);
+      // Don't hit the API too fast
+      if (offset && delay) {
+        await sleep(delay);
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+      clearTimeout(cId);
     }
   }
-
-  clearTimeout(cId);
 }
 
-function write(
+function writeData(
   baseData: Map<string, Record<string, unknown>[]> | unknown[],
   outputPath: string,
+  logger: typeof console,
 ): void {
   ensureDirSync(outputPath);
 
@@ -121,61 +138,79 @@ function write(
       const fileName = `${outputPath}/${tableName}.json`;
       try {
         Deno.writeTextFileSync(fileName, JSON.stringify(records));
-        console.log(`Records written to ${cyan(fileName)}`);
+        logger.log(`Records written to ${cyan(fileName)}`);
       } catch (e) {
-        console.error(red(`Could not write to ${cyan(fileName)}. Error: ${e}`));
+        logger.error(red(`Could not write to ${cyan(fileName)}. Error: ${e}`));
       }
     }
   } else {
     const fileName = `${outputPath}/LDA.json`;
     try {
       Deno.writeTextFileSync(fileName, JSON.stringify(baseData));
-      console.log(`Records written to ${cyan(fileName)}`);
+      logger.log(`Records written to ${cyan(fileName)}`);
     } catch (e) {
-      console.error(red(`Could not write to ${cyan(fileName)}. Error: ${e}`));
+      logger.error(red(`Could not write to ${cyan(fileName)}. Error: ${e}`));
     }
   }
 }
 
-const base = Deno.env.get("AIRTABLE_BASE_ID");
-const key = Deno.env.get("AIRTABLE_API_KEY");
+export async function main() {
+  const base = Deno.env.get("AIRTABLE_BASE_ID");
+  const key = Deno.env.get("AIRTABLE_API_KEY");
 
-const flags = parse(Deno.args);
+  const flags = parse(Deno.args);
 
-// Set flags if they are not set
-const httpReadTimeout: number = parseInt(flags.httpReadTimeout) || 60;
+  // Set flags if they are not set
+  const httpReadTimeout: number = parseInt(flags.httpReadTimeout) || 60;
 
-// Default to curl user agent to look basic
-const userAgent: string = flags.userAgent || "curl/7.77.0";
-const outputDir: string = flags.outputDir || "./dist";
+  // Default to curl user agent to look basic
+  const userAgent: string = flags.userAgent || "curl/7.77.0";
+  const outputDir: string = flags.outputDir || "./dist";
 
-const ALLTABLES = [
-  "Items",
-  "Composite_Objects",
-  "Entities",
-  "Locations",
-  "Subjects",
-  "Relationships",
-  "Item_Admin_Info",
-  "Person_Admin_Info",
-  "Lakeland_Book",
-  "Lakeland_Tour_Sites",
-];
+  const ALLTABLES = [
+    "Items",
+    "Composite_Objects",
+    "Entities",
+    "Locations",
+    "Subjects",
+    "Relationships",
+    "Item_Admin_Info",
+    "Person_Admin_Info",
+    "Lakeland_Book",
+    "Lakeland_Tour_Sites",
+  ];
 
-if (base && key) {
-  if (flags.backup) {
-    const allBaseData = await getAirtableData(
-      base,
-      ALLTABLES,
-      key,
-      httpReadTimeout,
-      userAgent,
+  if (base && key) {
+    if (flags.backup) {
+      try {
+        const allBaseData = await getAirtableData(
+          base,
+          ALLTABLES,
+          key,
+          httpReadTimeout,
+          fetch,
+          console,
+
+          userAgent,
+        );
+
+        writeData(allBaseData, join(outputDir, "data", "raw"), console);
+      } catch (error) {
+        console.error(red(`Unhandled error in getAirtableData: ${error}`));
+        Deno.exit(1);
+      }
+    }
+  } else {
+    console.error(
+      red("Error: AIRTABLE_BASE_ID and AIRTABLE_API_KEY must be set"),
     );
-    write(allBaseData, join(outputDir, "data", "raw"));
+    Deno.exit(1);
   }
-} else {
-  console.error(
-    red("Error: AIRTABLE_BASE_ID and AIRTABLE_API_KEY must be set"),
-  );
-  Deno.exit(1);
+}
+
+if (import.meta.main) {
+  main().catch((error) => {
+    console.error(red(`Unhandled error in main: ${error}`));
+    Deno.exit(1);
+  });
 }
